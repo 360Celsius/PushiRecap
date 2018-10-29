9f6a7f5efddb38a727db4eb0ac6d0a82d3caebc4
Shar, Dennis <dennis.shar@gmail.com>
2018-10-29 12:06:46 +0200

    update

--- PushiRecap/app/src/main/java/com/example/dennisshar/pushirecap/SplashActivity.java
+++ PushiRecap/app/src/main/java/com/example/dennisshar/pushirecap/SplashActivity.java
@@ -1,6 +1,8 @@
 package com.example.dennisshar.pushirecap;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends BaseActivity {
 
@@ -8,5 +10,16 @@ public class SplashActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                Intent intentone = new Intent(getApplicationContext(), MainActivity.class);
+                intentone.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intentone);
+            }
+        }, 5000);   //5 seconds
+
+
     }
 }
