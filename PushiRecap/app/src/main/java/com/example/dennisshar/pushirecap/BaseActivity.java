414b814f30b5d9d8061cbe7465f4d96054fe2dfe
Shar, Dennis <dennis.shar@gmail.com>
2018-10-29 09:11:13 +0200

    splash screen added

--- PushiRecap/app/src/main/java/com/example/dennisshar/pushirecap/BaseActivity.java
+++ PushiRecap/app/src/main/java/com/example/dennisshar/pushirecap/BaseActivity.java
@@ -1,7 +1,12 @@
 package com.example.dennisshar.pushirecap;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+
 import com.example.dennisshar.pushirecap.dbhelper.DatabaseHelper;
 import com.example.dennisshar.pushirecap.interfaces.DataBaseHelperInterface;
 
@@ -13,6 +18,18 @@ public class BaseActivity extends AppCompatActivity implements DataBaseHelperInt
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        Window window = getWindow();
+
+        // clear FLAG_TRANSLUCENT_STATUS flag:
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+        // add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+
+        // finally change the color
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.setStatusBarColor(ContextCompat.getColor(this, R.color.colorPrimary));
+        }
 
         helper = DatabaseHelper.getInstance(getApplicationContext());
 
